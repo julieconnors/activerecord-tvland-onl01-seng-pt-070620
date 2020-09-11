15c58c6c7b7c43d8cbfee22ba79cbdb6df789703
julieconnors <juliana.g.connors@gmail.com>
2020-09-11 18:53:49 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        characters.collect do |character_obj|
+            character_obj.actor.full_name
+        end
+    end
+
+    def build_network(network_letters)
+        self.network = Network.find_or_create_by(network_letters)
+    end
   
 end
